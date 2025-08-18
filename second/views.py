5050cbec0933c888fbc0f86898e8bf16cde9183b
jhpark0623 <pjhkny0113@gmail.com>
2025-08-18 13:00:42 +0900

    createsuperuser

--- second/views.py
+++ second/views.py
@@ -6,9 +6,9 @@ import json
 
 # Create your views here.
 def main(request):
-    violations = Violation.objects.all().values('latitude', 'longitude', 'address')
-
-    context = {
-        'violations': json.dumps(list(violations))
-    }
-    return render(request, 'main.html', context)
+    # violations = Violation.objects.all().values('latitude', 'longitude', 'address')
+    #
+    # context = {
+    #     'violations': json.dumps(list(violations))
+    # }
+    return render(request, 'main.html')
