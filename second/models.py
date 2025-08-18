a394dacf06379e7ea826736a0b78c68444d65c4f
suhyi <cajr0626@naver.com>
2025-08-18 12:33:36 +0900

    123

--- second/models.py
+++ second/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Violation(models.Model):
+    address = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
